ab841f5cbd23b120ed121162353bd5cf913d2667
Matthew Jourard <mjourard@mail.uoguelph.ca>
2015-11-24 14:25:48 -0500

    First iteration of linking with the database. Untested code. Still need to get the value of the radio buttons to populate the 'type' value of the json object

--- src/js/CreateTeamScript.js
+++ src/js/CreateTeamScript.js
@@ -1,3 +1,5 @@
+// Team Type Enum - 0 is public, 1 is private
+
 window.onload = function () {
 	document.getElementById("submit-button-href").parentNode.addEventListener("click", function() {
 		var nameTextBox = document.getElementById("team-name");
@@ -6,9 +8,16 @@ window.onload = function () {
 		var errorLabel = document.getElementById("team-name-error-message");
 		var captainErrorLabel = document.getElementById("team-captain-error-message");
 
+		var sendRequest = true;
+
+		/* Get which radio button is checked */
+		var radioButtons = document.getElementById("team-type");
+
+
 		if(nameTextBox.value == "") {
 			errorLabel.innerHTML = "Please enter the team name";
 			errorLabel.parentNode.classList.add("bg-danger");
+			sendRequest = false;
 		} else {
 			errorLabel.innerHTML = "";
 			errorLabel.parentNode.classList.remove("bg-danger");
@@ -17,13 +26,29 @@ window.onload = function () {
 		if (captainEmailBox.value == "") {
 			captainErrorLabel.innerHTML = "Please enter the team captain's email address";
 			captainEmailBox.parentNode.classList.add("bg-danger");
+			sendRequest = false;
 		} else {
 			captainErrorLabel.innerHTML = "";
 			captainEmailBox.parentNode.classList.remove("bg-danger");
 		}
 
-		if (nameTextBox.value != "" && captainEmailBox.value != "") {
+		/*if (nameTextBox.value != "" && captainEmailBox.value != "") {
 			window.location.replace("index.html");
-		}
+		}*/
+
+		var newTeamJson = {
+			"name" : nameTextBox.value,
+			"type" : 0,
+			"capId" : 40000,
+			"eventId" : 1
+		};
+
+		$.ajax ({
+		  type : 'POST',
+		  url : 'http://131.104.49.63/api/team/',
+		  data : newTeamJson
+		});
 	});
 }; /*79 Roehampton Crescent*/
+
+
